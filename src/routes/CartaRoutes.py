6577d910db3c56870bb7802c74a4f447cf9a772d
Mateus Pedrosa <mpedrosa001@gmail.com>
2024-12-01 23:56:36 -0300

    implementado endpoint de filtragem de cartas

--- src/routes/CartaRoutes.py
+++ src/routes/CartaRoutes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
+from typing import Optional, List
 from models.Carta import Carta
-from services.CartaService import verificar_carta_existe, adicionar_carta
+from services.CartaService import verificar_carta_existe, adicionar_carta, filtrar_cartas
 
 router = APIRouter()
 
@@ -11,3 +12,7 @@ def criar_carta(carta: Carta):
     
     adicionar_carta(carta)
     return {"message": "Carta adicionada com sucesso!"}
+
+@router.get("/filtrar/", response_model=List[Carta])
+def listar_cartas_filtradas(tipo: Optional[str] = None, cor: Optional[str] = None, rarity: Optional[str] = None):
+    return filtrar_cartas(tipo, cor, rarity)
